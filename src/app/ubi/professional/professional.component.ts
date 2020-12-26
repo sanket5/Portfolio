9e5dd2837b3d37d2eb4a63b278c34154d20ed911
sanket <jambhorkar.sanket05@gmail.com>
2020-12-26 18:42:21 +0530

    making dynamic layout

--- src/app/ubi/professional/professional.component.ts
+++ src/app/ubi/professional/professional.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, SkipSelf } from '@angular/core';
+import * as userData from '../demoData.json'
 
 @Component({
   selector: 'app-professional',
   templateUrl: './professional.component.html',
   styleUrls: ['./professional.component.css']
 })
-export class ProfessionalComponent implements OnInit {
-
+export class ProfessionalComponent implements OnInit, AfterViewInit {
+  displayData;
   constructor() { }
 
   ngOnInit(): void {
+    if (sessionStorage.getItem('userData')){
+      this.displayData = JSON.parse( sessionStorage.getItem('userData') )
+    }
+    else{
+      this.displayData = userData['default']
+    }
   }
+
+  ngAfterViewInit(){
+    this.displayData.skills.forEach(Skill => {
+      let sk = Skill.pro + "%"
+      console.log(Skill.skill);
+        let a = Skill.skill
+        document.getElementById(Skill.skill).style.width = sk
+        
+    });
+  }
+
+
+  
 
 }
